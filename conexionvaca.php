53d50119380932b9c5b860bec327055ded96c3fb
rdgzkei <2190005@ipisa.edu.do>
2023-05-05 13:38:39 -0400

    se actualizo la conexion

--- conexionvaca.php
+++ conexionvaca.php
@@ -1,4 +1,4 @@
 <?php
 global $conexionvaca;
-$conexionvaca = mysqli_connect('localhost', 'root', '', 'ipisa_php')or die(mysqli_error($mysqli));
+$conexionvaca = mysqli_connect('localhost', 'root', '12345', 'ipisa_php')or die(mysqli_error($mysqli));
 ?>
